95e5b3653fff73e0d7c85571de1fe8afecb8f56b
Ghazanfar-Perosonal <muhammadghazanfarhaider@gmail.com>
2026-05-13 18:04:06 +0500

    Add backend health API endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	  res.send("Backend Working");
+});
+
+app.get("/health", (req, res) => {
+	  res.json({
+		      success: true,
+		      message: "Backend Health API Working",
+		      timestamp: new Date(),
+		    });
 });
 
 app.listen(5000, () => {
